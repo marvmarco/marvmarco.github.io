e37f59edab5bc4c8661319ced6a47241401c642c
marvmarco <collepiccolo.marco@gmail.com>
2022-06-17 11:42:37 +0200

    modified

--- cv.js
+++
@@ -1,9 +0,0 @@
-document.getEventListener("DOMContentLoaded", ()=>{
-    cv_path = "https://github.com/marvmarco/marvmarco.github.io/raw/master/cv.pdf";
-    google_viewer = "https://docs.google.com/viewer?url="
-    url = google_viewer + cv_path
-    var cv_iframe = getElementById("cviframe")
-    cv_iframe.href = url
-    var cv_link = getElementById("cvlink")
-    cv_link.href = cv_path
-})
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", ()=>{
-    const body = document.querySelector("body")
-    let cv_link = document.createElement("a")
-    cv_link.href = url
-    let cv_link_text = document.createTextNode("CV")
-    cv_link.appendChild(cv_link_text)
-    body.appendChild(cv_link)
+    cv_url = "http://github.com/marvmarco/marvmarco.github.io/raw/master/cv.pdf"
+    google_viewer = "https://docs.google.com/gview?url="
+    url_gv_cv = google_viewer + cv_url + "&embedded=true"
+    var view_cv = document.getElementById("viewcv")
+    view_cv.href = url_gv_cv
+    var download_cv = document.getElementById("downloadcv")
+    download_cv.href = cv_url
 })

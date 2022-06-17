f3621c68f06dec6338299d635fa2ca4aa4e8c321
marvmarco <collepiccolo.marco@gmail.com>
2022-06-17 12:05:38 +0200

    fix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", ()=>{
-    cv_url = "http://github.com/marvmarco/marvmarco.github.io/raw/master/cv.pdf"
+    cv_url = "https://github.com/marvmarco/marvmarco.github.io/raw/master/cv.pdf"
     google_viewer = "https://docs.google.com/gview?url="
     url_gv_cv = google_viewer + cv_url + "&embedded=true"
     var view_cv = document.getElementById("viewcv")
